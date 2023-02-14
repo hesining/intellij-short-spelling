130d8ce17233363497c13aa8264571d799b85a69
GitHub Action <action@github.com>
2023-02-14 02:26:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/hesining/intellijshortspelling/MyBundle.kt
+++ src/main/kotlin/com/github/hesining/intellijshortspelling/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hesining.intellijshortspelling
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hesining/intellijshortspelling/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hesining/intellijshortspelling/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hesining.intellijshortspelling.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hesining.intellijshortspelling.MyBundle
 
 class MyApplicationService {
 
